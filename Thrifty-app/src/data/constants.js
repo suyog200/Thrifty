b0676b33fe6096284b52926db38717cd02fb70c1
udeshar <udesh.arlekar@gmail.com>
2024-04-05 20:08:57 +0530

    data in sorted

--- Thrifty-app/src/data/constants.js
+++ Thrifty-app/src/data/constants.js
@@ -1,5 +1,14 @@
-export const airlines =['SpiceJet','AirAsia','Vistara','GO_FIRST','Indigo','Air_India'];
-export const source_cities = ['Delhi','Mumbai','Bangalore','Kolkata','Hyderabad','Chennai']
-export const destination_cities = ['Mumbai','Bangalore','Kolkata','Hyderabad','Chennai','Delhi']
-export const classes = ['Economy','Business']
-export const arrival_times = ['Night','Morning','Early_Morning','Afternoon','Evening','Late_Night']
+const airlines_raw =['SpiceJet','AirAsia','Vistara','GO_FIRST','Indigo','Air_India'];
+export const airlines = airlines_raw.sort().map((airline, index) => ({label: airline, id: index}));
+
+const source_cities_raw = ['Delhi','Mumbai','Bangalore','Kolkata','Hyderabad','Chennai'];
+export const source_cities = source_cities_raw.sort().map((city, index) => ({label: city, id: index}));
+
+const destination_cities_raw = ['Mumbai','Bangalore','Kolkata','Hyderabad','Chennai','Delhi']
+export const destination_cities = destination_cities_raw.sort().map((city, index) => ({label: city, id: index}));
+
+const classes_raw = ['Economy','Business']
+export const classes = classes_raw.sort().map((class_type, index) => ({label: class_type, id: index}));
+
+// const arrival_times_raw = ['Night','Morning','Early_Morning','Afternoon','Evening','Late_Night'];
+// export const arrival_times = arrival_times_raw.sort().map((time, index) => ({label: time, id: index}));
