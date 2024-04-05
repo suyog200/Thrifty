b5080575a76cf1efe0dc2cc697de1548608504dd
suyog200 <suygchari@gmail.com>
2024-04-05 21:19:59 +0530

    added date picker and fixed bugs

--- Thrifty-app/src/components/InputBox.jsx
+++ Thrifty-app/src/components/InputBox.jsx
@@ -1,15 +1,21 @@
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
 
-export default function InputBox({id, label, Data}) { 
-    return (
-        <Autocomplete
-        disablePortal
-        id={id}
-        options={Data} //provide optiobs
-        sx={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label={label}/>}
-      />
-    );
+export default function InputBox({ id, label, Data }) {
+  return (
+    <Autocomplete
+      disablePortal
+      id={id}
+      options={Data} //provide optiobs
+      sx={{ width: 300 }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label={label}
+          variant="filled"
+          sx={{ backgroundColor: "#fff"}}
+        />
+      )}
+    />
+  );
 }
-  
--- Thrifty-app/src/components/InputForm.jsx
+++ Thrifty-app/src/components/InputForm.jsx
@@ -1,8 +1,18 @@
+import * as React from "react";
+import { useState } from "react";
 import InputBox from "./InputBox";
-import { airlines, destination_cities, source_cities } from "../data/constants";
+import dayjs from "dayjs";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { airlines, classes, destination_cities, source_cities } from "../data/constants";
 
 export default function InputForm() {
+  const [value, setValue] = useState(dayjs("2022-04-17"));
+
   return (
+    <>
     <div className="inputs">
       <InputBox id="airlines" label="Airlines" Data={airlines} />
       <InputBox id="source" label="Source" Data={source_cities} />
@@ -11,6 +21,23 @@ export default function InputForm() {
         label="Destination"
         Data={destination_cities}
       />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoContainer components={["DatePicker", "DatePicker"]} sx={{paddingTop: '0', overflow: 'visible'}}>
+            <DatePicker
+              label="Date of Journey"
+              value={value}
+              onChange={(newValue) => setValue(newValue)}
+              sx={{ backgroundColor: "#fff"}}
+
+            />
+          </DemoContainer>
+        </LocalizationProvider>
+        <InputBox
+        id="class"
+        label="Class"
+        Data={classes}
+         />
     </div>
+    </>
   );
 }
