5ff0303ff11bc4a7a11a63e91ea65150d2248ad3
udeshar <udesh.arlekar@gmail.com>
2024-04-05 19:48:46 +0530

    added data

---
+++ Thrifty-app/src/data/constants.js
@@ -0,0 +1,5 @@
+export const airlines =['SpiceJet','AirAsia','Vistara','GO_FIRST','Indigo','Air_India'];
+export const source_cities = ['Delhi','Mumbai','Bangalore','Kolkata','Hyderabad','Chennai']
+export const destination_cities = ['Mumbai','Bangalore','Kolkata','Hyderabad','Chennai','Delhi']
+export const classes = ['Economy','Business']
+export const arrival_times = ['Night','Morning','Early_Morning','Afternoon','Evening','Late_Night']
